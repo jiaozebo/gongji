4c27a270dfa9aa20ad17f2512e492d4374a8cfc8
jiaozebo <jiaozebo@tsinglink.com>
2018-05-21 19:55:18 +0800

    111

--- gongji.js
+++ gongji.js
@@ -1,10 +1,8 @@
 const net = require('net');
-var Iconv = require('iconv').Iconv;
+// var Iconv = require('iconv').Iconv;
 // const Buffer = require('buffer');
-
-
-
-var path = require('path');
+const util = require("util")
+const path = require('path');
 var root = path.join(__dirname, '..','public', 'log');
 console.log("path root : " + root);
 const fs = require('fs');
@@ -13,8 +11,7 @@ if (!fs.existsSync(root)) {
 }
 if (process.env.NODE_ENV == 'development'){var logger = require('tracer').colorConsole();}else{var logger = require('tracer').dailyfile({root: root, maxLogFiles: 10});}
 
-const IP = "127.0.0.1";
-const PORT = 7000;
+const EventEmitter = require('events');
 
 
 class GJHeader{
@@ -31,15 +28,14 @@ class GJHeader{
 } buffer 
      */
     constructor(buffer){
-        this.buffer = buffer;
         this.offset = 0;
-        this.baotou = this.readInt32LE();
-        this.version = this.readInt32LE();
-        this.cmd = this.readInt32LE();
-        this.bodyLen = this.readInt32LE();
+        this.baotou = this.readInt32LE(buffer);
+        this.version = this.readInt32LE(buffer);
+        this.cmd = this.readInt32LE(buffer);
+        this.bodyLen = this.readInt32LE(buffer);
     }
-    readInt32LE(){
-        let x = this.buffer.readInt32LE(this.offset);
+    readInt32LE(buffer){
+        let x = buffer.readInt32LE(this.offset);
         this.offset += 4;
         return x;        
     }
@@ -48,33 +44,32 @@ class GJHeader{
 class GJUnit{
     // 站点名称|事件来源|事件内容|事件级别|事件类型|发生时间|事件ID（即event_guid）|
     constructor(content){
-        this.key = content.substring(0, content.indexOf("|"))
-        content = content.substring(content.indexOf("||") + 2);
-        const arr = content.split("|");
-        this.站点名称 = arr[0]
-        this.事件来源 = arr[1]
-        this.事件内容 = arr[2]
-        this.事件级别 = arr[3]
-        this.事件类型 = arr[4]
-        this.发生时间 = arr[5]
-        this.事件ID = arr[6]
+        const firstDivider = content.indexOf("|");
+        this.key = content.substring(0, firstDivider);
+        this.value = content.substring(firstDivider + 1);
+        // const arr = content.split("|");
+        // this.站点名称 = arr[0]
+        // this.事件来源 = arr[1]
+        // this.事件内容 = arr[2]
+        // this.事件级别 = arr[3]
+        // this.事件类型 = arr[4]
+        // this.发生时间 = arr[5]
+        // this.事件ID = arr[6]
     }
 }
 
 class GJBody{
     constructor(buffer){
-        this.bodies = [];
-        let offset = 0;
-        let iconv = new Iconv('GB2312', 'UTF-8');
-        buffer = iconv.convert(buffer);
+        this.units = [];
+        // let iconv = new Iconv('GB2312', 'UTF-8');
+        // buffer = iconv.convert(buffer);
         while(buffer.length != 0){
-            let unitLen = buffer.readInt16LE(offset); offset += 2;
-            let unitStr = buffer.toString('utf8', offset, unitLen);
+            let unitLen = buffer.readInt16LE(0); 
+            let unitStr = buffer.toString('utf8', 2, unitLen + 2);
             let unit = new GJUnit(unitStr);
-            logger.info(unit);
-            this.bodies.push(unit);
+            
+            this.units.push(unit);
             buffer = buffer.slice(2+unitLen);
-            offset += unitLen;
         }
     }
 }
@@ -87,15 +82,40 @@ class GJ{
     }
 }
 
-const client = net.createConnection({ port: PORT, host:IP }, async () => {
-    logger.info("connect to server:%j", client);
+class GJManager extends EventEmitter{
+    constructor(ip, port){
+        super()
+        this.ip = ip;
+        this.port = port;
 
-});
+        // const gj = new GJ(Buffer.alloc(512));
+        const that = this;
+        const client = net.createConnection({ port: this.port, host:this.ip }, async () => {
+            logger.info("connect to server:%j",{ port: that.port, host:that.ip });
+            
+            let flag = 0x00;
 
-client.on("data", async (data)=>{
-    let gj = new GJ(data);
-});
+            let buf = Buffer.from([
+                0xFF,0xff,0xff,0xff,
+                0x02,0x00,0x00,0x01,
+                flag,0xff,0xff,0xff, 		// 命令类型  
+                0x00,0x00,0x00,0x00, 		// 包体长度                
+                0xFF,0xff,0xff,0xff,
+                0xFF,0xff,0xff,0xff,
+                0xFF,0xff,0xff,0xff,
+                0xFF,0xff,0xff,0xff,
+            ]);
+            
 
-module.exports = {
-    GJ:GJ,
+        });
+
+        client.on("data", async (data)=>{
+            const gj = new GJ(data);
+            that.emit("data", gj);
+        });
+
+        this.client = client;
+    }
 }
+
+module.exports = GJManager
